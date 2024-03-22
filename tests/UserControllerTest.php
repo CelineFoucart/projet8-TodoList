3ff7e6ae439e007e22e8294e5ba2afa2719748a1
Céline Foucart <celinefoucart@yahoo.fr>
2024-03-22 17:49:35 +0100

    add: test for usercontroller edit

--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
@@ -31,5 +33,29 @@ class UserControllerTest extends WebTestCase
         $this->loginUser($client, 'admin@gmail.com');
         $client->request('GET', '/users');
         $this->assertSelectorTextContains('h2', 'Liste des utilisateurs');
+    }
+
+    public function testEditAsAdmin(): void
+    {
+        $client = static::createClient();
+        $this->makeFixture();
+        $this->loginUser($client, 'admin@gmail.com');
+        $user = $this->getUser('johndoe@gmail.com');
+
+        $client->request('GET', '/users/' . $user->getId() . '/edit');
+        $this->assertSelectorTextContains('h2', 'Modifier le compte '.$user->getUsername());
+    }
+
+    protected function getUser(string $email): User
+    {
+        /** @var UserRepository */
+        $repository = static::getContainer()->get(UserRepository::class);
+        $task = $repository->findOneBy(['email' => $email]);
+
+        if (null === $task) {
+            throw new \Exception('No user named');
+        }
+
+        return $task;
     }
 }
